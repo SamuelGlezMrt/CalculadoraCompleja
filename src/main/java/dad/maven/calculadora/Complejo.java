13e6d5e75584e94e5e91e7a2e2d0b156e4711570
SamuelGlezMrt <71700577+SamuelGlezMrt@users.noreply.github.com>
2021-11-17 09:54:11 +0000

    Update Complejo.java

--- src/main/java/dad/maven/calculadora/Complejo.java
+++ src/main/java/dad/maven/calculadora/Complejo.java
@@ -36,4 +36,52 @@ public class Complejo {
 		this.imaginarioProperty().set(imaginario);
 	}
 
+	public Complejo sumar(Complejo suma) {
+
+		Complejo sum = new Complejo();
+		sum.realProperty().bind(realProperty().add(suma.realProperty()));
+		sum.imaginarioProperty().bind(imaginarioProperty().add(suma.imaginarioProperty()));
+
+		return sum;
+
+	}
+
+	public Complejo restar(Complejo resta) {
+
+		Complejo res = new Complejo();
+		res.realProperty().bind(realProperty().subtract(resta.realProperty()));
+		res.imaginarioProperty().bind(imaginarioProperty().subtract(resta.imaginarioProperty()));
+
+		return res;
+
+	}
+
+	public Complejo muliplicacion(Complejo multicar) {
+
+		Complejo multi = new Complejo();
+		multi.realProperty().bind(realProperty().multiply(multicar.realProperty())
+				.subtract(imaginarioProperty().multiply(multicar.imaginarioProperty())));
+		multi.imaginarioProperty().bind(realProperty().multiply(multicar.imaginarioProperty())
+				.add(imaginarioProperty().multiply(multicar.realProperty())));
+		return multi;
+
+	}
+
+	public Complejo division(Complejo dividir) {
+		Complejo div = new Complejo();
+		div.realProperty()
+				.bind((realProperty().multiply(dividir.realProperty())
+						.add(imaginarioProperty().multiply(dividir.imaginarioProperty())))
+								.divide(dividir.realProperty().multiply(dividir.realProperty())
+										.add(dividir.imaginarioProperty().multiply(dividir.imaginarioProperty()))));
+
+		div.imaginarioProperty()
+				.bind((imaginarioProperty().multiply(dividir.realProperty())
+						.subtract(realProperty().multiply(dividir.imaginarioProperty())))
+								.divide(dividir.realProperty().multiply(dividir.realProperty())
+										.add(dividir.imaginarioProperty().multiply(dividir.imaginarioProperty()))));
+
+		return div;
+	}
+
 }
